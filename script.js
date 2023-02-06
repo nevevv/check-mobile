ea96e8a98f7199224f36d36a2dfbcc84984a9718
nevevv <103154860+nevevv@users.noreply.github.com>
2023-02-06 19:46:41 +0500

    css

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ const button = document.querySelector("button");
 
 btn.addEventListener("click", () => {
   modal.style.transform = "translateY(-50%) translateX(-50%)";
-  body.style.opacity = '.3'
+  body.style.opacity = ".3";
 });
 
 button.addEventListener("click", () => {
   modal.style.transform = "translateY(-300vh) translateX(-50%)";
-  body.style.opacity = '1'
-
+  body.style.opacity = "1";
 });
