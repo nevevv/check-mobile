8c9033d467edf291df3e789a984ab38cf6fe45ca
nevevv <103154860+nevevv@users.noreply.github.com>
2023-02-06 16:33:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector(".advise");
+const modal = document.querySelector(".modal");
+const body = document.querySelector(".container");
+const button = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  modal.style.transform = "translateY(-50%) translateX(-50%)";
+  body.style.filter = "blur(10px)";
+});
+
+button.addEventListener("click", () => {
+  modal.style.transform = "translateY(-300vh) translateX(-50%)";
+  body.style.filter = "blur(0)";
+  location.reload();
+});
